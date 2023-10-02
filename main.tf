eb33ec20e9eb024ca823b2949d1613a2b783b129
Mike <michael.flannery@gmail.com>
2023-10-02 15:07:57 -0400

    added main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "mikedemo1"
+
+    workspaces {
+      name = "Acme-aws"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0bb4c991fa89d4b9b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ACME_Demo_App_Server"
+  }
+}
